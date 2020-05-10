45bae46baa4193edfbb58d4fd894906946c61dc0
Ben Soutendijk <devaent1316@gmail.com>
2020-05-09 19:18:37 -0500

    todos api

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,17 +1,19 @@
 import mongoose from 'mongoose';
 
 export enum TodoStatus {
+    Todo,
     Doing,
     Done,
     Archived,
 }
 
 export interface Todo extends mongoose.Document {
-    title: string,
-    description: string,
-    status: TodoStatus,
-    createdOn: Date,
-    updatedOn: Date,
+    title: string;
+    description: string;
+    status: TodoStatus;
+    createdOn: Date;
+    updatedOn: Date;
+    userid: string;
 }
 
 const todoSchema = new mongoose.Schema({
@@ -19,7 +21,8 @@ const todoSchema = new mongoose.Schema({
     description: { type: String, required: true },
     status: { type: TodoStatus, required: true },
     createdOn: { type: Date, required: true },
-    updatedOn: { type: Date, required: false },
+    updatedOn: { type: Date, required: true },
+    userid: { type: String, required: true },
 });
 
 mongoose.model<Todo>('Todo', todoSchema);
--- src/routes/api/todos/index.ts
+++ src/routes/api/todos/index.ts
@@ -1,26 +1,113 @@
 import express, { Request, Response } from "express";
+import mongoose from 'mongoose';
 import auth from "../../../auth";
+import Todo, { TodoStatus } from "../../../models/Todo";
 
 const router = express.Router();
 
+const Todo = mongoose.model<Todo>('Todo');
+
 router.post('/', ...auth.required, async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send('Create new Todo');
+    const { user, body } = req;
+
+    try {
+        const todo = new Todo({
+            title: body.title,
+            description: body.description,
+            userid: user._id,
+            createdOn: new Date().getTime(),
+            updatedOn: new Date().getTime(),
+            status: TodoStatus.Todo
+        });
+
+        await todo.save();
+        return res.status(200).send(todo.toJSON());
+    } catch (error) {
+        return res.status(400).send({ message: error.message });
+    }
 })
 
 router.get('/', ...auth.required, async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send('Get all Todos');
+    const { user } = req;
+
+    try {
+        const todos = await Todo.find({ 
+            userid: user._id,
+        });
+        return res.status(200).send(todos);
+    } catch (error) {
+        return res.status(400).send({ message: error.message });
+    }
 });
 
-router.get('/:todoid', ...auth.required, async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send('Get Todo by ID');
+router.get('/:takeid', ...auth.required, async (req: Request, res: Response): Promise<Response> => {
+    const { user, params } = req;
+
+    try {
+        const take = await Todo.findOne({ 
+            _id: params.takeid,
+            userid: user._id,
+        });
+
+        if (take === null) {
+            throw new Error('take not found')
+        }
+
+        return res.status(200).send(take);
+    } catch (error) {
+        return res.status(400).send({ message: error.message });
+    }
 });
 
-router.post('/:todoid', ...auth.required, async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send('Update Todo');
-})
+router.post('/:takeid', ...auth.required, async (req: Request, res: Response): Promise<Response> => {
+    const { user, body, params } = req;
 
-router.delete('/:todoid', ...auth.required, async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send('Delete Todo');
-})
+    try {
+        const take = await Todo.findOne({ 
+            _id: params.takeid, 
+            userid: user._id,
+        });
+
+        if (take === null) {
+            throw new Error('take not found')
+        }
+
+        Object.assign(take, {
+            title: body.title,
+            description: body.description,
+            status: body.status,
+            updatedOn: new Date().getTime(),
+        });
+
+        await take.save();
+        return res.status(200).send(take);
+    } catch (error) {
+        return res.status(400).send({ message: error.message });
+    }
+});
+
+router.delete('/:takeid', ...auth.required, async (req: Request, res: Response): Promise<Response> => {
+    const { user, params } = req;
+
+    try {
+        const take = await Todo.findOne({ 
+            _id: params.takeid, 
+            userid: user._id
+        });
+
+        if (take === null) {
+            throw new Error('take not found')
+        }
+
+        Object.assign(take, {
+            status: TodoStatus.Archived,
+        });
+
+        await take.save();
+        return res.status(200).send(take);
+    } catch (error) {
+        return res.status(400).send({ message: error.message });
+    }
+});
 
 export default router;
