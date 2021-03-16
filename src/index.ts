55bedb7e8e0b1f942b4c54c75a071a8e558e55b4
Ben Soutendijk <bensoutendijk@gmail.com>
2021-03-15 21:23:15 -0500

    remove fluff

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,18 @@
+import path from 'path';
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import passport from 'passport';
-import keys from './config/keys';
 
-// Connect to Database
-mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useFindAndModify: false });
-require('./models/LocalUser');
-require('./models/OAuthUser');
-require('./models/Card');
-require('./models/Board');
-require('./models/Category');
-require('./services/passport');
+// Load Environment Variables
+require('dotenv').config(path.resolve(__dirname, '../.env'));
 
 // Create express app
 import routes from './routes';
 const app = express();
 
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(routes);
 
 const server = http.createServer(app);
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,14 +1,9 @@
 import express from 'express';
-import authRoutes from './auth';
-import cardsRoutes from './cards';
-import boardsRoutes from './boards'
-import categoriesRoutes from './categories'
 
 const router = express.Router();
 
-router.use('/auth', authRoutes);
-router.use('/cards', cardsRoutes);
-router.use('/boards', boardsRoutes);
-router.use('/categories', categoriesRoutes);
+router.get('/hello', (req, res) => {
+  return res.send('Hello World');
+});
 
 export default router;
